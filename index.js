02a7764552921ff4d2354ce76c4f8e70aea4fb3b
sanku <sanna.kukkonen2@metropolia.fi>
2024-02-15 13:33:35 +0200

    3.4: puhelinluettelon backend step 4

--- index.js
+++ index.js
@@ -37,10 +37,14 @@ app.get('/api/contacts/:id', (req, res) => {
     const contact = contacts.find(c => c.id===id)
     contact
         ? res.json(contact)
-        : res.status(404).end()
+        : res.status(404).end() // not found
 })
 
-
+app.delete('/api/contacts/:id', (req, res) => {
+    const id = Number(req.params.id)
+    contacts = contacts.filter(c => c.id !== id)
+    res.status(204).end()   // no content
+})
 
 const PORT = 3001
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
