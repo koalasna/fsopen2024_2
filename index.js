5490bb7b6930253723af49e30fc20ffbedd61cce
sanku <sanna.kukkonen2@metropolia.fi>
2024-03-07 22:45:05 +0200

    3.16-17: puhelinluettelo ja tietokanta, step 4-5

--- index.js
+++ index.js
@@ -12,19 +12,18 @@ app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 morgan.token('body', function (req, res) {return JSON.stringify(req.body)})
 
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+    if(error.name === 'CastError')
+        return res.status(400).send({ error: 'malformatted id'})
+}
+
 app.get('/api/contacts', (req, res) => 
     Contact.find({})
         .then(contacts => res.json(contacts))    
 )
 
-/*
-app.get('/info', (req, res) => 
-    res.send(`
-    <p>Phonebook has info for ${contacts.length} people</p>
-    <h2>${new Date().toString()}</h2>`)
-) */
-
-app.get('/api/contacts/:id', (req, res) => {
+app.get('/api/contacts/:id', (req, res, next) => {
     Contact.findById(req.params.id)
         .then(contact => {
             if(contact) 
@@ -32,20 +31,17 @@ app.get('/api/contacts/:id', (req, res) => {
             else 
                 res.status(404).end()            
         })
-        .catch(error => {
-            console.log(error)
-            res.status(400).send({error: 'malformatted id'})
-        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/contacts/:id', (req, res) => {
+app.delete('/api/contacts/:id', (req, res, next) => {
     Contact.findByIdAndDelete(req.params.id)
         .then(result => 
             res.status(204).end())
-            // tähän vielä .catch(error=>next(error))
+        .catch(error => next(error))    
 })
 
-app.post('/api/contacts', (req, res) => {
+app.post('/api/contacts', (req, res, next) => {
    const body = req.body
 
     if(!body.name)
@@ -58,20 +54,33 @@ app.post('/api/contacts', (req, res) => {
             error: 'number missing'
     })
 
-/*    palaa tähän
-    if(contacts.map(c=>c.name).includes(body.name))
-        return res.status(400).json({
-            error: 'name already exists'
-    }) */
-
     const contact = new Contact({
         name: body.name,
         number: body.number
     })
     
-    contact.save().then(savedContact =>
+    contact
+      .save()
+      .then(savedContact =>
         res.json(savedContact))
+      .catch(error => next(error))
 })
+
+app.put('/api/contacts/:id', (req, res, next) => {
+    const body = req.body
+
+    const contact = {
+        name: body.name,
+        number: body.number
+    }
+
+    Contact.findByIdAndUpdate(req.params.id, contact, {new: true})
+        .then(updatedContact => res.json(updatedContact))
+        .catch(error => next(error))
+})
+
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
